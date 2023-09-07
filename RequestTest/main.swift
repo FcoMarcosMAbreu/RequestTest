80f9bc30e9b1502e685c3e8e18f5d7cd18902244
Marcos Abreu <MAbr88@ProtonMail.com>
2023-09-07 20:50:26 -0300

    Up

--- RequestTest/main.swift
+++ RequestTest/main.swift
@@ -19,36 +19,20 @@ struct Country: Codable {
     }
 }
 
-struct Countries: Decodable {
-    let data: [Country]
-}
-
 func fetchRemoteCountries() async throws -> [Country] {
     let url = URL(string: "https://apiv3.apifootball.com/?action=get_countries&APIkey=fe56acd2acb45d94a9f1331149dc55aa2846896f38ecf1484de36d6a3cea87f0")!
 
     let request = URLRequest(url: url)
     
     let (data, _) = try await URLSession.shared.data(for: request)
-        //data.forEach(country in {
-        //print(country.rawValue)
-    //})
-    
-    let decoder = JSONDecoder()
-    
-    return try decoder.decode([Country].self, from: data)
+
+    return try JSONDecoder().decode([Country].self, from: data)
 }
 
 var results = try await fetchRemoteCountries()
-
-//print(results.data.count)
-//print(results.data.self)
 
 results.forEach { country in
     print(country.countryID)
     print(country.countryName)
     print(country.countryLogo)
 }
-
-
-print("Hello, World!")
-
